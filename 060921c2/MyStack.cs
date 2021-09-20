12dce1c14fa8e69af58249ed084363e7a796c138
Werenyckyj <werenyckyjvojtech@gmail.com>
2021-09-20 15:15:28 +0200

    Přidejte soubory projektu.

---
+++ 060921c2/MyStack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _060921c2
+{
+    class MyStack
+    {
+        static char[] p;
+        static int index;
+        public MyStack()
+        {
+            p = new char[1000];
+            index = -1;
+        }
+        public static void Push(char c)
+        {
+            p[++index] = c;
+        }
+        public static char Pop()
+        {
+            char c = p[index];
+            --index;
+            return c;
+        }
+        public static char Peek()
+        {
+            char c = p[index];
+            return c;
+        }
+        public static bool IsEmpty()
+        {
+            if (index==-1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static string PrintStack()
+        {
+            string s = "";
+            for (int i = 0; i < index; i++)
+            {
+                s += p[index] + " ";
+            }
+            return s;
+        }
+    }
+}
---
+++ 060921c2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _060921c2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zavorky = "{}[]()";
+            string notok = "3+2{3+[4(2+])}+2"; string ok = "3+2{3+[4(2)]}+2";
+            Console.WriteLine(notok);
+            MyStack x = new MyStack();
+            for (int i = 0; i < notok.Length; i++)
+            {
+               char c = notok[i];
+                if (c==zavorky[0]|| c == zavorky[2]|| c == zavorky[4])
+                {
+                    x.Push(c);
+                }
+                if (c == zavorky[1] || c == zavorky[3] || c == zavorky[5])
+                {
+                    char ch = x.Peek();
+                    if (ch==c)
+                    {
+                        x.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Chyba!!!");
+                    }
+                }
+            }
+            Console.WriteLine(ok);
+
+            Console.ReadLine();
+        }
+    }
+}
